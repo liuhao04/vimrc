00ffed1e58b7147b165236ca832241b05c7f891d
liuhao04 <liuhao04@gmail.com>
2017-12-11 08:28:29 +0800

    update tmux config script

--- tmux_conf.sh
+++ tmux_conf.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/bash
  
- cp ~/.tmux.conf ~/.tmux.conf.bak.`date '%Y%m%d%H%M'`
- cat ~/tmux.conf > ~/.tmux.conf
- source ~/.tmux.conf
+ cp ~/.tmux.conf ~/.tmux.conf.bak.`date '+%Y%m%d%H%M'`
+ cat tmux.conf > ~/.tmux.conf
